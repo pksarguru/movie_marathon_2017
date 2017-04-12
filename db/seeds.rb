5b1284c7969ac205c3867bf9d03a75388eef9d89
Logan Price <logandsprice@gmail.com>
2017-04-12 18:18:31 -0500

    add summary seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,3 +4,7 @@ User.create(username: "Logan", password: "Logan", password_confirmation: "Logan"
 movies = ["The Godfather", "The Last Samurai", "Gone Baby Gone", "Blade Runner", "2001: A Space Odyssey", "Step Brothers"]
 
 movies.each {|title| Movie.create(title: title)}
+
+pavan.summaries.create(text:"POW!", movie_id: 1)
+pavan.summaries.create(text:"PAH!", movie_id: 2)
+logan.summaries.create(text:"beep boop!", movie_id: 1)
