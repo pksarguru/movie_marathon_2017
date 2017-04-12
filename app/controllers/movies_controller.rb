3d6c66b187a0892af5a9ca5926ece791cf2322a8
Logan Price <logandsprice@gmail.com>
2017-04-12 18:18:07 -0500

    add preliminary logic for movies#show

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,6 +4,10 @@ class MoviesController < ApplicationController
   end
 
   def show
-
+    if Movie.find(params["id"]).in?(current_user.summarized_movies)
+      print "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+    else
+      print "NOPENOPENOPENOPENOPENOPENOPE"
+    end
   end
 end
