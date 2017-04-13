20a3e57264ae5cd3915c45862c8a8cdf380205f1
Pavan Sarguru <pavansarguru@gmail.com>
2017-04-12 20:35:35 -0500

    add scoreboard values

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,7 @@
 class MoviesController < ApplicationController
   def index
     if user_signed_in?
+      @users = User.all
       @movies = Movie.all
     else
       redirect_to new_user_registration_path
